b7bb040646d89ba74a42c22b224c168269ff96a0
xiexianhui14 <729217673@qq.com>
2015-09-06 14:17:28 +0800

    测试修改

--- TestHub/AppDelegate.m
+++ TestHub/AppDelegate.m
@@ -16,6 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    //测试
     // Override point for customization after application launch.
     return YES;
 }
